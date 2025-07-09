7c6fda6d6f07434ae4dc9120d4392a811b76682f
Inco <paveliv2015@gmail.com>
2025-07-09 13:22:48 +0200

    Getting ready
    getting ready for work
    
    Co-Authored-By: NaufalVerse <203447250+NaufalVerse@users.noreply.github.com>

--- src/test/java/com/crimsonwarpedcraft/exampleplugin/ExamplePluginTest.java
+++ src/test/java/com/crimsonwarpedcraft/exampleplugin/ExamplePluginTest.java
@@ -1,16 +1,2 @@
 package com.crimsonwarpedcraft.exampleplugin;
-
 import org.junit.jupiter.api.Test;
-
-/**
- * Tests for ExamplePlugin.
- *
- * @author Copyright (c) Levi Muniz. All Rights Reserved.
- */
-public class ExamplePluginTest {
-
-  @Test
-  public void onEnable() {
-
-  }
-}
