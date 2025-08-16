a5bde886a8760f0b2a6d5971750bbe12ca6f166a
Inco <paveliv2015@gmail.com>
2025-08-16 17:18:13 +0200

    Update IClanTest.java
    quicker testing

---
+++ src/test/java/com/naufalverse/iclan/IClanTest.java
@@ -0,0 +1,11 @@
+package com.naufalverse.iclan;
+
+import org.junit.jupiter.api.Test;
+
+public class IClanTest {
+
+    @Test
+    void testPluginBoot() {
+        System.out.println("If you see this message the plugin (\"iClan\") was installed and booted successfully.");
+    }
+}
